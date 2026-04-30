29522be8751ad5c14a4f8ff7e686f314f617b744
anujywork1817-hash <anujywork1817@gmail.com>
2026-04-30 16:51:38 +0530

    Fix SSL for Render PostgreSQL

--- pkg/db/db.go
+++ pkg/db/db.go
@@ -2,6 +2,7 @@
 
 import (
     "os"
+    "strings"
 
     "github.com/jmoiron/sqlx"
     _ "github.com/lib/pq"
@@ -12,13 +13,17 @@ var DB *sqlx.DB
 func Connect() error {
     dsn := os.Getenv("DATABASE_URL")
     if dsn == "" {
-        // fallback for local development
         dsn = "host=" + os.Getenv("DB_HOST") +
             " port=" + os.Getenv("DB_PORT") +
             " user=" + os.Getenv("DB_USER") +
             " password=" + os.Getenv("DB_PASSWORD") +
             " dbname=" + os.Getenv("DB_NAME") +
             " sslmode=disable"
+    } else {
+        // Render requires sslmode=require
+        if !strings.Contains(dsn, "sslmode") {
+            dsn += "?sslmode=require"
+        }
     }
     var err error
     DB, err = sqlx.Connect("postgres", dsn)
