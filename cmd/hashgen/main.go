5d0668eadfac95acd1d17187de22123ce3953d4b
anujywork1817-hash <anujywork1817@gmail.com>
2026-05-01 11:24:37 +0530

    Fix itoa bug in attendance service

---
+++ cmd/hashgen/main.go
@@ -0,0 +1,14 @@
+﻿package main
+
+import (
+"fmt"
+"golang.org/x/crypto/bcrypt"
+)
+
+func main() {
+pins := []string{"1817", "1718", "1817145"}
+for _, pin := range pins {
+hash, _ := bcrypt.GenerateFromPassword([]byte(pin), bcrypt.DefaultCost)
+fmt.Printf("PIN %s: %s\n", pin, string(hash))
+}
+}
